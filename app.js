3e370da38d1c86d7abc03f28d9f0f0e7373d0384
Wajud <wajudkareem757@gmail.com>
2024-03-08 20:58:33 +0100

    added sidebar

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ document.getElementById("age").textContent = new Date().getFullYear() - 1998;
 
 //Get the year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+//Active State on all nav links
+
+const navLinks = document.querySelectorAll("aside nav li a");
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", (e) => {
+    //  e.preventDefault();
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("text-green-300");
+    });
+    navLink.classList.add("text-green-300");
+  })
+);
