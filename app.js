3cb6612be6cb499cbe8033b6ae549073f08ff5f3
Wajud <wajudkareem757@gmail.com>
2024-08-10 05:33:35 +0100

    removed age, added updated resume, added new email

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Get my age
 
-document.getElementById("age").textContent = new Date().getFullYear() - 1998;
+// document.getElementById("age").textContent = new Date().getFullYear() - 1998;
 
 //Get the year
 document.getElementById("year").textContent = new Date().getFullYear();
